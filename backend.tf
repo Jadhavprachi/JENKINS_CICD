3c185c59c47024edcde22a8bba570e16df3957b0
prachi jadhav <pjprachi2312@gmail.com>
2023-12-07 15:03:42 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket"
+    bucket         = "myproject-cicd"
     key            = "my-terraform-environment/main"
     region         = "us-east-1"
-    dynamodb_table = "mrcloudbook-dynamo-db-table"
+    dynamodb_table = "myproject-cicd-dynamo-db-table"
   }
 }
