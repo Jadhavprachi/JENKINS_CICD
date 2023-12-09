bbef731a491c8fb68cfea8b5b223db63d98c30db
prachi jadhav <pjprachi2312@gmail.com>
2023-12-09 10:53:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "prachi" {
+resource "aws_instance" "Ajay" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name
